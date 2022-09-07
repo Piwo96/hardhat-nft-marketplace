934049cf9ecb3e09c57355fc28b79b269254f342
Piwo96 <philipp.rollmann@online.de>
2022-09-07 18:43:28 +0200

    added mint and list script

---
+++ scripts/mint-and-list.ts
@@ -0,0 +1,43 @@
+import { ethers } from "hardhat";
+import { BasicNftOne, NftMarketplace } from "../typechain-types";
+
+const PRICE = ethers.utils.parseEther("0.1");
+
+async function mintAndList() {
+    const accounts = await ethers.getSigners();
+    const deployer = accounts[0];
+    const nft: BasicNftOne = await ethers.getContract("BasicNftOne", deployer);
+    const nftMarketplace: NftMarketplace = await ethers.getContract(
+        "NftMarketplace",
+        deployer
+    );
+
+    // Mint nft
+    console.log("Minting ...");
+    const mintTx = await nft.mintNft();
+    const mintTxReceipt = await mintTx.wait(1);
+    const tokenId = mintTxReceipt.events![1].args!.tokenId;
+
+    // Approving nft
+    console.log("Approving NFT ...");
+    const approvalTx = await nft.approve(nftMarketplace.address, tokenId);
+    await approvalTx.wait(1);
+
+    // List nft
+    console.log("Listing the NFT ...");
+
+    const listingTx = await nftMarketplace.listItem(
+        nft.address,
+        tokenId,
+        PRICE
+    );
+    await listingTx.wait(1);
+    console.log("Listed!");
+}
+
+mintAndList()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    });
