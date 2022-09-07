966bf701df851df58a19f7214152b10cf39b37d6
Piwo96 <philipp.rollmann@online.de>
2022-09-07 09:47:53 +0200

    cleaned up

--- deploy/00-deploy-mocks.ts
+++ deploy/00-deploy-mocks.ts
@@ -1,29 +1,25 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { developmentChains } from "../helper-hardhat-config";
-import { BasicNftOne, BasicNftTwo } from "../typechain-types";
 
 const deployMocks: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-    const { network, getNamedAccounts, deployments } = hre;
+    const { getNamedAccounts, deployments } = hre;
     const { deployer } = await getNamedAccounts();
     const { deploy, log } = deployments;
 
-    if (developmentChains.includes(network.name)) {
-        console.log("Deploying Basic Nfts ...");
-        await deploy("BasicNftOne", {
-            contract: "BasicNftOne",
-            from: deployer,
-            log: true,
-        });
+    log("Deploying Basic Nfts ...");
+    await deploy("BasicNftOne", {
+        contract: "BasicNftOne",
+        from: deployer,
+        log: true,
+    });
 
-        await deploy("BasicNftTwo", {
-            contract: "BasicNftTwo",
-            from: deployer,
-            log: true,
-        });
-        console.log("Nfts deployed!");
-        console.log("------------------------------------");
-    }
+    await deploy("BasicNftTwo", {
+        contract: "BasicNftTwo",
+        from: deployer,
+        log: true,
+    });
+    log("Nfts deployed!");
+    log("------------------------------------");
 };
 
 export default deployMocks;
