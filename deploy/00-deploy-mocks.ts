fd4cf9e52bd41f4ce7530af6ac92623618aa6fee
Piwo96 <philipp.rollmann@online.de>
2022-09-07 09:46:30 +0200

    finished

--- deploy/00-deploy-mocks.ts
+++ deploy/00-deploy-mocks.ts
@@ -7,6 +7,23 @@ const deployMocks: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const { network, getNamedAccounts, deployments } = hre;
     const { deployer } = await getNamedAccounts();
     const { deploy, log } = deployments;
+
+    if (developmentChains.includes(network.name)) {
+        console.log("Deploying Basic Nfts ...");
+        await deploy("BasicNftOne", {
+            contract: "BasicNftOne",
+            from: deployer,
+            log: true,
+        });
+
+        await deploy("BasicNftTwo", {
+            contract: "BasicNftTwo",
+            from: deployer,
+            log: true,
+        });
+        console.log("Nfts deployed!");
+        console.log("------------------------------------");
+    }
 };
 
 export default deployMocks;
